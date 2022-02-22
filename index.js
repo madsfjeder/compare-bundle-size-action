aca3ef9b034b99cb1889abc76f983ff5df3b183e
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-02-22 11:30:09 +0100

    First commit

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const base_path = core.getInput('base_path');
+  console.log(`base_path: ${base_path}!`);
+  console.log(JSON.parse(base_path))
+
+  const pr_path = core.getInput('pr_path');
+  console.log(`pr_path: ${pr_path}!`);
+  console.log(JSON.parse(pr_path))
+
 } catch (error) {
   core.setFailed(error.message);
 }
