8c0cfa6aec71200387d5c1c4e43ba6e4ce44fb0d
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-02-22 14:32:12 +0100

    Changes

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  const base_path = core.getInput('base_path');
-  console.log(`base_path: ${base_path}!`);
-  console.log(JSON.parse(base_path))
-
   const pr_path = core.getInput('pr_path');
-  console.log(`pr_path: ${pr_path}!`);
-  console.log(JSON.parse(pr_path))
+  const rawPr = fs.readFileSync(pr_path);
+  const pr = JSON.parse(rawPr);
+
+  console.log(pr)
+
+  const prInMb = pr.bytes / 1024 / 1024
+
+  core.setOutput("pr_size_mb", prInMb);
+
+  const base_path = core.getInput('base_path');
+  const rawBase = fs.readFileSync(base_path);
+  const base = JSON.parse(base);
+
+  console.log(base)
+
+  const baseInMb = base.bytes / 1024 / 1024
+
+  core.setOutput("base_size_mb", baseInMb);
 
 } catch (error) {
   core.setFailed(error.message);
