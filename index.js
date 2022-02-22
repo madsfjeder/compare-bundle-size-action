587d3569bb184246198835a99d3d0224f23461ee
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-02-22 15:35:51 +0100

    Changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 try {
   const pr_path = core.getInput('pr_path');
-  const rawPr = fs.readFileSync(pr_path);
+  const rawPr = fs.readFileSync(pr_path + '/bundle_size.json');
   const pr = JSON.parse(rawPr);
 
   console.log(pr)
@@ -14,15 +14,17 @@ try {
   core.setOutput("pr_size_mb", prInMb);
 
   const base_path = core.getInput('base_path');
-  const rawBase = fs.readFileSync(base_path);
+  const rawBase = fs.readFileSync(base_path + '/bundle_size.json');
   const base = JSON.parse(base);
 
   console.log(base)
 
   const baseInMb = base.bytes / 1024 / 1024
+  const percentChange = (prInMb - baseInMb) / baseInMb * 100
 
   core.setOutput("base_size_mb", baseInMb);
-
+  core.setOutput("percentDifference", percentChange);
+  core.setOutput('diff', prInMb-baseInMb);
 } catch (error) {
   core.setFailed(error.message);
 }
