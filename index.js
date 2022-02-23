9b58c205eeed2b08423f342f6b09c3820764a903
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-02-23 08:51:49 +0100

    Changes

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const fs = require('fs');
 
 try {
   const pr_path = core.getInput('pr_path');
   const rawPr = fs.readFileSync(pr_path + '/bundle_size.json');
   const pr = JSON.parse(rawPr);
-
-  console.log(pr)
-
   const prInMb = pr.bytes / 1024 / 1024
-
-
 
   const base_path = core.getInput('base_path');
   const rawBase = fs.readFileSync(base_path + '/bundle_size.json');
   const base = JSON.parse(rawBase);
-
-  console.log(base)
-
   const baseInMb = base.bytes / 1024 / 1024
-  console.log(baseInMb)
-  const percentChange = (prInMb - baseInMb) / baseInMb * 100
-  core.setOutput("pr_size", prInMb.toString());
-  core.setOutput("base_size", baseInMb.toString());
-  core.setOutput("percentDifference", percentChange.toString());
-  core.setOutput('diff', (prInMb-baseInMb).toString());
+
+  const diff = prInMb - baseInMb;
+  const percentChange = (diff) / baseInMb * 100
+
+  core.setOutput("pr_size", prInMb.toPrecision(4));
+  core.setOutput("base_size", baseInMb.toPrecision(4));
+  core.setOutput("percentDifference", percentChange.toPrecision(4));
+  core.setOutput('diff', (diff).toPrecision(4));
 } catch (error) {
   core.setFailed(error.message);
 }
